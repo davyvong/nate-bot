8ae12be4fe2f31778f85231d8896464b565d706d
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-04-28 11:28:40 -0400

    feat: generate image for weather api

---
+++ app/api/weather/route.tsx
@@ -0,0 +1,154 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { ImageResponse } from '@vercel/og';
+import OpenWeatherAPI from 'apis/openweather';
+import { Units } from 'apis/openweather/enums';
+import { object, string } from 'yup';
+
+export const runtime = 'experimental-edge';
+
+const fonts = [
+  fetch(new URL('../../../assets/fonts/inter-medium.ttf', import.meta.url)).then(response => response.arrayBuffer()),
+  fetch(new URL('../../../assets/fonts/inter-regular.ttf', import.meta.url)).then(response => response.arrayBuffer()),
+];
+
+const renderPrediction = (prediction: OpenWeatherPrediction): JSX.Element => (
+  <div
+    key={prediction.dayOfWeek + prediction.timeOfDay}
+    style={{
+      alignItems: 'center',
+      backgroundColor: '#23272a',
+      borderRadius: 8,
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      margin: 8,
+      marginTop: 0,
+    }}
+  >
+    <div style={{ fontSize: 16, fontWeight: 500 }}>{prediction.dayOfWeek + ' ' + prediction.timeOfDay}</div>
+    <div style={{ color: '#99aab5', fontSize: 14, marginTop: 8 }}>{prediction.weather}</div>
+    <img alt="" height="100" src={prediction.icon} width="100" />
+    <div style={{ fontSize: 24, fontWeight: 500 }}>{prediction.temperature.actual + '°C'}</div>
+    <div style={{ color: '#99aab5', fontSize: 14, marginTop: 8 }}>
+      {'Feels like ' + prediction.temperature.feelsLike + '°C'}
+    </div>
+  </div>
+);
+
+export async function GET(request: Request) {
+  const requestURL = new URL(request.url);
+  const params = {
+    city: requestURL.searchParams.get('city'),
+    country: requestURL.searchParams.get('country'),
+    state: requestURL.searchParams.get('state'),
+  };
+  const paramsSchema = object({
+    city: string().required().min(1).max(100),
+    country: string().required().length(2),
+    state: string().required().min(0).max(100),
+  });
+  if (!paramsSchema.isValidSync(params)) {
+    return new Response(undefined, { status: 400 });
+  }
+  const location = await OpenWeatherAPI.getLocation(params.city, params.state, params.country);
+  if (!location) {
+    return new Response(undefined, { status: 404 });
+  }
+  let units = Units.Celsius;
+  if (['LR', 'MM', 'US'].includes(location.country)) {
+    units = Units.Fahrenheit;
+  }
+  const forecast = await OpenWeatherAPI.getForecast(location.latitude, location.longitude, units);
+
+  const [interMedium, interRegular] = await Promise.all(fonts);
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#2c2f33',
+          color: '#ffffff',
+          display: 'flex',
+          flexDirection: 'column',
+          fontFamily: 'Inter',
+          height: '100%',
+          justifyContent: 'center',
+          padding: 8,
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: 16,
+            marginTop: 8,
+            width: '100%',
+          }}
+        >
+          <img alt="" height="100" src={forecast.predictions[0].icon} width="100" />
+          <div
+            style={{
+              display: 'flex',
+              flex: 1,
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <div style={{ color: '#99aab5', fontSize: 16 }}>{forecast.city + ', ' + forecast.country}</div>
+            <div style={{ fontSize: 24, fontWeight: 500, marginTop: 8 }}>
+              {forecast.predictions[0].dayOfWeek + ' ' + forecast.predictions[0].timeOfDay}
+            </div>
+            <div style={{ color: '#99aab5', fontSize: 16, marginTop: 8 }}>Sunny</div>
+          </div>
+          <div
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              marginLeft: 26,
+              marginRight: 16,
+            }}
+          >
+            <div style={{ fontSize: 52, lineHeight: 1 }}>12°C</div>
+            <div style={{ color: '#99aab5', fontSize: 16, marginTop: 8 }}>Feels like 10°C</div>
+          </div>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'row',
+            width: '100%',
+          }}
+        >
+          {forecast.predictions.slice(1, 4).map(renderPrediction)}
+        </div>
+      </div>
+    ),
+    {
+      fonts: [
+        {
+          data: interRegular,
+          name: 'Inter',
+          style: 'normal',
+          weight: 400,
+        },
+        {
+          data: interMedium,
+          name: 'Inter',
+          style: 'normal',
+          weight: 500,
+        },
+      ],
+      height: 390,
+      width: 600,
+    },
+  );
+}
