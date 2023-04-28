7208eaca404e21b2698afa65bc27354599fefc3b
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-04-28 20:53:28 +0000

    fix: set discord interactions api to node runtime

--- app/api/discord/interactions/route.tsx
+++ app/api/discord/interactions/route.tsx
@@ -1,9 +1,7 @@
 import DiscordClient from 'clients/discord';
 
-export const runtime = 'experimental-edge';
-
 export async function POST(request: Request) {
-  const isVerified = DiscordClient.verifyInteraction(request);
+  const isVerified = await DiscordClient.verifyInteraction(request);
   if (!isVerified) {
     return new Response('invalid request signature', { status: 404 });
   }
