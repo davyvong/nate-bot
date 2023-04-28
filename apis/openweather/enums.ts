cf174bc159d2a7e05ca4f097a6f3e96c81ddf46f
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-04-27 22:00:42 -0400

    feat: add discord client and weather api (#2)
    * feat: add discord client and openweather api
    
    * feat: add weather api route

---
+++ apis/openweather/enums.ts
@@ -0,0 +1,25 @@
+export enum Day {
+  Sunday = 'Sunday',
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
+}
+
+export enum TimeOfDay {
+  Midnight = 'Midnight',
+  Overnight = 'Overnight',
+  Morning = 'Morning',
+  Noon = 'Noon',
+  Afternoon = 'Afternoon',
+  Evening = 'Evening',
+  Night = 'Night',
+}
+
+export enum Units {
+  Celsius = 'metric',
+  Fahrenheit = 'imperial',
+  Kelvin = 'standard',
+}
