047d0506ca2abefd112c2655b4f7e18c3c520ee0
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-05-03 19:36:15 -0400

    feat: add current weather api (#18)

--- apis/openweather/types.d.ts
+++ apis/openweather/types.d.ts
@@ -9,10 +9,10 @@ interface OpenWeatherLocation {
 interface OpenWeatherForecast {
   city: string;
   country: string;
-  predictions: OpenWeatherPrediction[];
+  predictions: OpenWeatherSnapshot[];
 }
 
-interface OpenWeatherPrediction {
+interface OpenWeatherSnapshot {
   dayOfWeek: string;
   icon: string;
   temperature: {
@@ -90,4 +90,57 @@ interface OpenWeatherForecastResponse {
     sunrise?: number;
     sunset?: number;
   };
+}
+
+interface OpenWeatherCurrentWeatherResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level?: number;
+    grnd_level?: number;
+  };
+  visibility?: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust?: number;
+  };
+  rain?: {
+    '1h': number;
+    '3h'?: number;
+  };
+  snow?: {
+    '1h': number;
+    '3h'?: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type?: number;
+    id?: number;
+    country: string;
+    sunrise?: number;
+    sunset?: number;
+  };
+  timezone?: number;
+  id: number;
+  name: string;
+  cod: number;
 }
