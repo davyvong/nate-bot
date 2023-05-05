6a09fb1c8d02b5c6ba2ef4b18f4d192502e4622d
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-05-05 18:31:43 +0000

    fix: inngest api exports

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,4 +1,4 @@
 import InngestAPI from 'apis/inngest';
 import { serve } from 'inngest/next';
 
-export default serve(InngestAPI.getInstance(), InngestAPI.createFunctions());
+export const { GET, POST, PUT } = serve(InngestAPI.getInstance(), InngestAPI.createFunctions());
