19a6444b2e1d6a221ea6cc6bea3398df25888abe
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-05-20 23:50:47 -0400

    feat: add discord auth verify to planetscale api

--- app/api/crons/list/route.ts
+++ app/api/crons/list/route.ts
@@ -1,7 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import DiscordAuthentication from 'server/discord/authentication';
 import PlanetScaleAPI from 'server/planetscale/api';
 
-export const GET = async () => {
+export const GET = async (request: NextRequest) => {
+  const token = await DiscordAuthentication.verifyToken(request.cookies);
+  if (!token) {
+    return new Response(undefined, { status: 401 });
+  }
   const results = await PlanetScaleAPI.getInstance().execute('SHOW TABLES');
   return NextResponse.json(results);
 };
