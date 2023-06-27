7255eacde785d438c7374162cf19c63c7ddf0ebd
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-06-27 20:13:50 +0000

    feat: add weatherman reaction

--- docker/app.ts
+++ docker/app.ts
@@ -1,7 +1,7 @@
 import { Client, GatewayDispatchEvents, GatewayIntentBits } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
 import { WebSocketManager } from '@discordjs/ws';
-import { fetch } from 'undici';
+import { Response, fetch } from 'undici';
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN as string);
 
@@ -13,26 +13,21 @@ const gateway = new WebSocketManager({
 
 const client = new Client({ rest, gateway });
 
+const createReaction = async (channelId: string, messageId: string, emoji: string): Promise<Response> => {
+  return fetch(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}/reactions/${emoji}/@me`, {
+    method: 'PUT',
+    headers: {
+      Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
+    },
+  });
+};
+
 client.on(GatewayDispatchEvents.MessageCreate, async ({ data }) => {
   try {
     if (data.mentions.some(mention => mention.id === process.env.DISCORD_BOT_ID)) {
-      const channelId = data.channel_id;
-      const messageId = data.id;
-      const emoji = 'bulba:715627372191285328';
-      console.log({ channelId, messageId, emoji });
-      const response = await fetch(
-        `https://discord.com/api/v10/channels/${channelId}/messages/${messageId}/reactions/${emoji}/@me`,
-        {
-          method: 'PUT',
-          headers: {
-            Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
-          },
-        },
-      );
-      console.log({
-        status: response.status,
-        statusText: response.statusText,
-      });
+      console.log(data);
+      await createReaction(data.channel_id, data.id, 'bulba:715627372191285328');
+      await createReaction(data.channel_id, data.id, 'WeatherMan:1117920707469398158');
     }
   } catch (error: unknown) {
     if (error instanceof Error) {
