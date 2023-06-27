3ef4260f0c4645afc9bc4f4833f3bf3da90fab55
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-06-27 20:22:00 +0000

    feat: remove bulba reaction

--- docker/app.ts
+++ docker/app.ts
@@ -1,7 +1,7 @@
 import { Client, GatewayDispatchEvents, GatewayIntentBits } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
 import { WebSocketManager } from '@discordjs/ws';
-import { Response, fetch } from 'undici';
+import { fetch } from 'undici';
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN as string);
 
@@ -13,21 +13,19 @@ const gateway = new WebSocketManager({
 
 const client = new Client({ rest, gateway });
 
-const createReaction = async (channelId: string, messageId: string, emoji: string): Promise<Response> => {
-  return fetch(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}/reactions/${emoji}/@me`, {
-    method: 'PUT',
-    headers: {
-      Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
-    },
-  });
-};
-
 client.on(GatewayDispatchEvents.MessageCreate, async ({ data }) => {
   try {
     if (data.mentions.some(mention => mention.id === process.env.DISCORD_BOT_ID)) {
       console.log(data);
-      await createReaction(data.channel_id, data.id, 'bulba:715627372191285328');
-      await createReaction(data.channel_id, data.id, 'WeatherMan:1117920707469398158');
+      const channelId = data.channel_id;
+      const messageId = data.id;
+      const emoji = 'WeatherMan:1117920707469398158';
+      await fetch(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}/reactions/${emoji}/@me`, {
+        method: 'PUT',
+        headers: {
+          Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
+        },
+      });
     }
   } catch (error: unknown) {
     if (error instanceof Error) {
