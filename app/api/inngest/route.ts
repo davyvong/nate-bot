025e1ba3b999b85242e1a28accea97b872b10025
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-07-03 11:04:11 -0400

    feat: set runtime to edge for inngest api

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,4 +1,8 @@
 import { serve } from 'inngest/next';
 import InngestAPI from 'server/inngest/api';
 
-export const { GET, POST, PUT } = serve(InngestAPI.getInstance(), InngestAPI.createFunctions());
+export const runtime = 'edge';
+
+export const { GET, POST, PUT } = serve(InngestAPI.getInstance(), InngestAPI.createFunctions(), {
+  streaming: 'allow',
+});
